6680526635a8ddcc069f54e1e3a09df624d22787
vitor <vitorn2009@hotmail.com>
2020-08-06 23:29:21 -0300

    Create table connections - Rota para criar e listar conexões - Filtro para as aulas - Controller para as connections

--- src/controllers/ClassesController.ts
+++ src/controllers/ClassesController.ts
@@ -73,6 +73,28 @@ export default class ClassesController {
             })
         }
 
-       // const timeInMinutes = convertHourToMinutes(filters.time)
+        const subject = filters.subject as string
+        const week_day = filters.week_day as string
+        const time = filters.time as string
+
+        const timeInMinutes = convertHourToMinutes(time)
+
+        const classes = await db('classes')
+            .whereExists(function() {
+                this.select('class_schedule.*')
+                    .from('class_schedule')    
+                    .whereRaw('`class_schedule`.`class_id` = `classes`.`id`')
+                    .whereRaw('`class_schedule`.`week_day` = ??',[Number(week_day)])
+                    .whereRaw('`class_schedule`.`from` <= ??',[timeInMinutes])
+                    .whereRaw('`class_schedule`. `to` > ??',[timeInMinutes])
+            })
+            .where('classes.subject', '=', subject)
+            .join('users', 'classes.user_id', '=', 'users.id')
+            .select(['classes.*', 'users.* '])
+
+        console.log(timeInMinutes)
+
+        return response.json(classes)
+
     }
 }
